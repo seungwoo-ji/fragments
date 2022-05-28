99c3a2acb5e1383f0145eaf6da33710525634250
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2022-05-28 19:16:53 -0400

    Added app.js unit test

---
+++ tests/unit/app.test.js
@@ -0,0 +1,15 @@
+// tests/unit/app.test.js
+
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('Error Handling Middlewares', () => {
+  test('requesting not-found resources should return HTTP 404 response', async () => {
+    const res = await request(app).get('/invalid-url');
+    expect(res.statusCode).toBe(404);
+    expect(res.body.status).toBe('error');
+    expect(res.body.error.message).toBe('not found');
+    expect(res.body.error.code).toBe(404);
+  });
+});
