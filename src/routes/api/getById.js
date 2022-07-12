d8c1bf7fc4437924db719d48c8a4db2ac0a07e3f
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2022-07-12 09:18:10 -0400

    Utilized path and mime modules to parse fragment id

--- src/routes/api/getById.js
+++ src/routes/api/getById.js
@@ -1,38 +1,30 @@
+const path = require('path');
+const mime = require('mime-types');
+
 const logger = require('../../logger');
 const { Fragment } = require('../../model/fragment');
 const { createErrorResponse } = require('../../response');
-
-const typeExtensions = {
-  txt: 'text/plain',
-  md: 'text/markdown',
-  html: 'text/html',
-  json: 'application/json',
-  png: 'image/png',
-  jpg: 'image/jpeg',
-  webp: 'image/webp',
-  gif: 'image/gif',
-};
 
 /**
  * Get an authenticated user's fragment data
  */
 module.exports = async (req, res) => {
-  const [id, extension] = req.params.id.split('.');
+  const { name, ext } = path.parse(req.params.id);
 
   try {
-    const fragment = await Fragment.byId(req.user, id);
+    const fragment = await Fragment.byId(req.user, name);
     const data = await fragment.getData();
-    logger.debug({ data }, `fragment data is found for user=${req.user} id=${id}`);
+    logger.debug({ data }, `fragment data is found for user=${req.user} fragment id=${name}`);
 
-    if (!extension) {
+    if (!ext) {
       res.set('content-type', fragment.type);
       res.status(200).send(data);
 
       return;
     }
 
-    if (fragment.formats.includes(typeExtensions[extension])) {
-      res.set('content-type', typeExtensions[extension]);
+    if (fragment.formats.includes(mime.lookup(ext))) {
+      res.set('content-type', fragment.type);
       res.status(200).send(data);
     } else {
       logger.error(
@@ -51,6 +43,8 @@ module.exports = async (req, res) => {
     }
   } catch (error) {
     logger.error(error.message);
-    res.status(404).json(createErrorResponse(404, `id: ${id} does not represent a known fragment`));
+    res
+      .status(404)
+      .json(createErrorResponse(404, `fragment id: ${name} does not represent a known fragment`));
   }
 };
