36ac095ac25b5cf191102c424a0e6236bc5c59fe
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2022-07-13 00:21:39 -0400

    Updated get route with expand query string

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,11 +1,18 @@
 // src/routes/api/get.js
 
+const { Fragment } = require('../../model/fragment');
+const logger = require('../../logger');
 const { createSuccessResponse } = require('../../response');
 
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
-  // TODO: this is just a placeholder to get something working...
-  res.status(200).json(createSuccessResponse({ fragments: [] }));
+module.exports = async (req, res) => {
+  try {
+    const data = await Fragment.byUser(req.user, req.query.expand);
+    logger.debug({ data }, `fragment list data is found for user=${req.user}`);
+    res.status(200).json(createSuccessResponse({ fragments: data }));
+  } catch (error) {
+    logger.error(error.message);
+  }
 };
